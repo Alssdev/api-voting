0ad9c7c0b1e1e469e8a70ed156077848be6c1f78
subuyugaby <subuyu.gaby@gmail.com>
2023-05-02 10:02:50 -0600

    feat:Creación de establecimientos

---
+++ src/establecimientos/establecimientos.routes.ts
@@ -0,0 +1,62 @@
+import { Router, Request, Response } from "express";
+import sql from "./../database"
+import response from "../types/tipos";
+
+
+export default (): Router => {
+  const router = Router();
+  router.post("/", async function (req: Request, res: Response) {
+    try {
+      let request = req.body as Req.Establecimientos;
+      await sql`INSERT INTO establecimientos ${sql(request)}`;
+      res.sendStatus(200);
+    } catch (error) {
+      res.sendStatus(500);
+      console.log(error);
+    }
+  })
+
+  router.get("/", async function (req: Request, res: Response) {
+    try {
+      let response = await sql`SELECT * FROM establecimientos`
+      res.json({
+        list: response,
+      })
+    } catch (error) {
+      res.sendStatus(500);
+    }
+  })
+
+  router.get("/:idest", async function (req: Request, res: Response) {
+    try {
+      let response = await sql`SELECT * FROM establecimientos WHERE idest = ${req.params.idest}`
+      res.json({
+        list: response,
+      })
+    } catch (error) {
+      res.sendStatus(500);
+    }
+  })
+
+  router.delete("/:idest", async function (req: Request, res: Response) {
+    try {
+      await sql`DELETE FROM establecimientos WHERE idest = ${req.params.idest}`;
+      res.sendStatus(200);
+    } catch (error) {
+      res.sendStatus(500);
+    }
+  })
+
+  router.put("/:idest", async function (req: Request, res: Response) {
+    try {
+      let request = req.body as Req.Establecimientos;
+      await sql`UPDATE establecimientos SET ${sql(request,"direccion","idmunicipio","nombre")}  WHERE idest = ${req.params.idest}`;
+      res.sendStatus(200);
+    } catch (error) {
+      res.sendStatus(500);
+      console.log(error);
+    }
+  })
+
+  return router;
+};
