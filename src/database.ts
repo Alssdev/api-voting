eb09fccb090b29b6a12776925fd1c3e077537ae3
Alessandro <alssdev3@gmail.com>
2023-05-02 12:41:52 -0600

    chore(cors): Cors agregado a la dependecias

--- src/database.ts
+++ src/database.ts
@@ -1,10 +1,12 @@
 import postgres from "postgres";
-const sql = postgres("postgres://postgres:gabyss22@host:5432/ccv",{
+
+const password = process.env.PASSWORD ? process.env.PASSWORD : 'gabyss22';
+const sql = postgres(`postgres://postgres:${password}@host:5432/ccv`,{
   host:'localhost',
   port: 5432,
   database:'ccv',
   username:'postgres',
-  password:'gabyss22',
+  password,
+});
 
-})
 export default sql;
