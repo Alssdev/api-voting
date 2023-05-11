33fd98582f976c4c0516dd919cccbfe3e56ff529
subuyugaby <subuyu.gaby@gmail.com>
2023-05-10 22:44:03 -0600

    feat: nuevas rutas para candidatos

--- src/candidatos/candidatos.routes.ts
+++ src/candidatos/candidatos.routes.ts
@@ -1,7 +1,6 @@
 import { Router, Request, Response } from "express";
 import sql from "./../database"
-import {  leerMunicipio,  leerCiudadano, leerTipoCandidato, leerPartido} from '../helpers/funciones';
-
+import { leerMunicipio, leerCiudadano, leerTipoCandidato, leerPartido } from '../helpers/funciones';
 
 
 
@@ -10,7 +9,7 @@ export default (): Router => {
   router.post("/", async function (req: Request, res: Response) {
     try {
       let request = req.body as Req.Candidatos;
-      await sql`INSERT INTO candidatos ${sql(request,"idemp","idpartido","casilla","tipo","idmunicipio","iddep")}`;
+      await sql`INSERT INTO candidatos ${sql(request, "idemp", "idpartido", "casilla", "tipo", "idmunicipio", "iddep")}`;
       res.sendStatus(200);
     } catch (error) {
       res.sendStatus(500);
@@ -20,15 +19,15 @@ export default (): Router => {
 
   router.get("/", async function (req: Request, res: Response) {
     try {
-      let response = await  sql<Req.Candidatos[]>`SELECT * FROM candidatos ORDER BY idemp`;
-      for(let candidato of response){
+      let response = await sql<Req.Candidatos[]>`SELECT * FROM candidatos ORDER BY idemp`;
+      for (let candidato of response) {
         let municipio = await leerMunicipio(candidato.idmunicipio);
         let tipo = await leerTipoCandidato(candidato.tipo);
         let partido = await leerPartido(candidato.idpartido);
         let ciudadanos = await leerCiudadano(candidato.idemp);
-        candidato.municipios= municipio;
+        candidato.municipios = municipio;
         candidato.tipoCandidatos = tipo;
-        candidato.partido= partido;
+        candidato.partido = partido;
         candidato.ciudadanos = ciudadanos;
       }
       res.json({
@@ -47,9 +46,9 @@ export default (): Router => {
       let tipo = await leerTipoCandidato(response[0].tipo);
       let partido = await leerPartido(response[0].idpartido);
       let ciudadanos = await leerCiudadano(response[0].idemp);
-      response[0].municipios= municipio;
+      response[0].municipios = municipio;
       response[0].tipoCandidatos = tipo;
-      response[0].partido= partido;
+      response[0].partido = partido;
       response[0].ciudadanos = ciudadanos;
       res.json({
         list: response,
@@ -58,6 +57,128 @@ export default (): Router => {
       res.sendStatus(500);
     }
   })
+
+  router.get("/:idpartido/binomio", async function (req: Request, res: Response) {
+    try {
+      let responseP = await sql`SELECT * FROM candidatos WHERE idpartido = ${req.params.idpartido} AND tipo ='P'`
+      let responseV = await sql`SELECT * FROM candidatos WHERE idpartido = ${req.params.idpartido} AND tipo ='V'`
+      res.json({
+        presidente: {
+          idemp: responseP[0].idemp,
+        },
+        vicepresidente: {
+          idemp: responseV[0].idemp,
+        }
+      })
+    } catch (error) {
+      res.sendStatus(500);
+    }
+  })
+
+  router.get("/:idpartido/alcaldes", async function (req: Request, res: Response) {
+    try {
+      try {
+        let response = await sql<Req.Candidatos[]>`SELECT * FROM candidatos WHERE idpartido = ${req.params.idpartido} AND tipo='A'`;
+        let alcaldes: Array<any> = [];
+        for (let candidato of response) {
+          let municipio = await leerMunicipio(candidato.idmunicipio);
+          let ciudadano = await leerCiudadano(candidato.idemp);
+          alcaldes.push({
+            municipio: municipio,
+            ciudadano: ciudadano
+          })
+        }
+        res.json({
+          alcaldes
+        });
+      } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+      }
+    } catch (error) {
+      console.log(error)
+      res.sendStatus(500);
+    }
+  })
+
+  router.get("/:idpartido/alcaldes", async function (req: Request, res: Response) {
+    try {
+      try {
+        let response = await sql<Req.Candidatos[]>`SELECT * FROM candidatos WHERE idpartido = ${req.params.idpartido} AND tipo='A'`;
+        let alcaldes: Array<any> = [];
+        for (let candidato of response) {
+          let municipio = await leerMunicipio(candidato.idmunicipio);
+          let ciudadano = await leerCiudadano(candidato.idemp);
+          alcaldes.push({
+            municipio: municipio,
+            ciudadano: ciudadano
+          })
+        }
+        res.json({
+          alcaldes
+        });
+      } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+      }
+    } catch (error) {
+      console.log(error)
+      res.sendStatus(500);
+    }
+  })
+
+  router.get("/:idpartido/:iddep/diputado_distrito", async function (req: Request, res: Response) {
+    try {
+      try {
+        let response = await sql<Req.Candidatos[]>`SELECT * FROM candidatos WHERE idpartido = ${req.params.idpartido} AND iddep = ${req.params.iddep} AND tipo = 'D'`;
+        let diputados: Array<any> = [];
+        for (let candidato of response) {
+          let ciudadanos = await leerCiudadano(candidato.idemp);
+          candidato.ciudadanos = ciudadanos;
+          diputados.push({
+            casilla: candidato.casilla,
+            ciudadano: ciudadanos
+          })
+        }
+        res.json({
+          diputados: diputados,
+        })
+      } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+      }
+    } catch (error) {
+      console.log(error)
+      res.sendStatus(500);
+    }
+  })
+
+  router.get("/:idpartido/diputado_listado_nacional", async function (req: Request, res: Response) {
+    try {
+      try {
+        let response = await sql<Req.Candidatos[]>`SELECT * FROM candidatos WHERE idpartido = ${req.params.idpartido}  AND tipo = 'N'`;
+        let diputados: Array<any> = [];
+        for (let candidato of response) {
+          let ciudadanos = await leerCiudadano(candidato.idemp);
+          candidato.ciudadanos = ciudadanos;
+          diputados.push({
+            casilla: candidato.casilla,
+            ciudadano: ciudadanos
+          })
+        }
+        res.json({
+          diputados: diputados,
+        })
+      } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+      }
+    } catch (error) {
+      console.log(error)
+      res.sendStatus(500);
+    }
+  })
+
 
   router.delete("/:idemp", async function (req: Request, res: Response) {
     try {
@@ -71,7 +192,7 @@ export default (): Router => {
   router.put("/:idemp", async function (req: Request, res: Response) {
     try {
       let request = req.body as Req.Candidatos;
-      await sql`UPDATE candidatos SET ${sql(request,"idpartido","casilla","tipo","idmunicipio","iddep")}  
+      await sql`UPDATE candidatos SET ${sql(request, "idpartido", "casilla", "tipo", "idmunicipio", "iddep")}  
                 WHERE idemp = ${req.params.idemp}`;
       res.sendStatus(200);
     } catch (error) {
